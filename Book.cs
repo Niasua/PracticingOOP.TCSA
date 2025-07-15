bfbbde776b438f1f1cef9e133ad55198c7aa5830
niasua <nsuar@DESKTOP-G227FH7>
2025-07-15 15:31:38 -0300

    Creates Magazine and Newspaper classes

--- Book.cs
+++ Book.cs
@@ -1,13 +1,33 @@
-﻿namespace TCSA.OOP.LibraryManagementSystem;
+﻿using Spectre.Console;
+using System.Reflection.Metadata;
+using TCSA.OOP.LibraryManagementSystem.Models;
 
-internal class Book
+namespace TCSA.OOP.LibraryManagementSystem;
+
+internal class Book : LibraryItem
 {
-    public string Name { get; set; } = "Unknown";
-    public int Pages { get; set; } = 0;
+    public string Author { get; set; }
+    public string Category { get; set; }
+    public int Pages { get; set; }
 
-    internal Book(string name, int pages)
+    internal Book(int id, string name, string author, string category, string location, int pages) : base(id, name, location)
     {
-        Name = name;
+        Author = author;
+        Category = category;
         Pages = pages;
     }
+
+    public override void DisplayDetails()
+    {
+        var panel = new Panel(new Markup($"[bold]Book:[/] [cyan]{Name}[/] by [cyan]{Author}[/]") +
+                                      $"\n[bold]Pages:[/] {Pages}" +
+                                      $"\n[bold]Category:[/] [green]{Category}[/]" +
+                                      $"\n[bold]Location:[/] [blue]{Location}[/]")
+        {
+            Border = BoxBorder.Rounded
+        };
+
+        AnsiConsole.Write(panel);
+    }
+
 }
