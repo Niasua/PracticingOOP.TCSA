6eb5068be14d20004d95edfd433d5b3833939f74
niasua <nsuar@DESKTOP-G227FH7>
2025-07-14 23:37:32 -0300

    Use tuples

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using Spectre.Console;
-using TCSA.OOP.LibraryManagementSystem;
+﻿//using Spectre.Console;
+//using TCSA.OOP.LibraryManagementSystem;
 
-UserInterface userInterface = new();
-userInterface.MainMenu();
+//UserInterface userInterface = new();
+//userInterface.MainMenu();
+
+(string Title, int Pages) book1 = ("Frankenstein", 350);
+Console.WriteLine($"Title: {book1.Title}, Pages: {book1.Pages}");
